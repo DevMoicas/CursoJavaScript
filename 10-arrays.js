c2ce642c629d65a1cc3f60e9fb77eae0c0c1cf9c
MoicasDev <grimaldogarcia2004@gmail.com>
2025-06-05 22:22:14 -0600

    I completed the DataStructures exercices

--- 10-arrays.js
+++ 10-arrays.js
@@ -1,70 +1,71 @@
-//array
+// //array
 
-//declarar array
+// //declarar array
 
-let myArray2 = new Array()
-let myArray = []
+// let myArray2 = new Array()
+// let myArray = []
 
-myArray = ["Moises", "Grimaldo", "Moicas", 29, false]
-myArray2 = new Array("Moises","Grimaldo", "Moicas", 20, true)
+// myArray = ["Moises", "Grimaldo", "Moicas", 29, false]
+// myArray2 = new Array("Moises","Grimaldo", "Moicas", 20, true)
 
-console.log(myArray2)
-console.log(myArray)
+// console.log(myArray2)
+// console.log(myArray)
 
-myArray2 = new Array(3)
-myArray2[0] = "Moises"
-//myArray2[1] = "Grimaldo"
-myArray2[2] = "Moicas"
+// myArray2 = new Array(3)
+// myArray2[0] = "Moises"
+// //myArray2[1] = "Grimaldo"
+// myArray2[2] = "Moicas"
 
-console.log(myArray2)
+// console.log(myArray2)
 
-myArray = []
-myArray[0] = "Moises"
-//myArray[1] = "Grimaldo"
-myArray[2] = "Moicas"
+// myArray = []
+// myArray[0] = "Moises"
+// //myArray[1] = "Grimaldo"
+// myArray[2] = "Moicas"
 
-console.log(myArray)
-
-
-//metodos comunes en arrays
-myArray = []
-
-//push y pop
-myArray.push("moises")
-myArray.push("grimaldo")
-myArray.push(20) //agrega al final del array
-
-console.log(myArray)
-
-// console.log(myArray.pop())
-// myArray.pop() //elimina al final del array
-
-console.log(myArray)
-
-//shift y unshift
-
-console.log(myArray.shift()) //shift elimina el primer elemento del array
-
-console.log(myArray)
-
-console.log(myArray.unshift("moises", "alejandro")) //agrega elementos al principio
-
-console.log(myArray)
-
-//length
-
-console.log(myArray.length)
-
-//clear
-myArray = []
-myArray = ["Moises", "Grimaldo", "Moicas", 29, false]
-//slice
-
-let newArray = myArray.slice(0, 2)  //devuelve una copia de una porcion del array
-console.log(newArray)
+// console.log(myArray)
 
 
-//splice
+// //metodos comunes en arrays
+// myArray = []
 
-myArray.splice(1,3, "AGREGAMOS UN NUEVO ELEMENTO")
-console.log(myArray)
+// //push y pop
+// myArray.push("moises")
+// myArray.push("grimaldo")
+// myArray.push(20) //agrega al final del array
+
+// console.log(myArray)
+
+// // console.log(myArray.pop())
+// // myArray.pop() //elimina al final del array
+
+// console.log(myArray)
+
+// //shift y unshift
+
+// console.log(myArray.shift()) //shift elimina el primer elemento del array
+
+// console.log(myArray)
+
+// console.log(myArray.unshift("moises", "alejandro")) //agrega elementos al principio
+
+// console.log(myArray)
+
+// //length
+
+// console.log(myArray.length)
+
+// //clear
+// myArray = []
+// myArray = ["Moises", "Grimaldo", "Moicas", 29, false]
+// //slice
+
+// let newArray = myArray.slice(0, 2)  //devuelve una copia de una porcion del array
+// console.log(newArray)
+
+
+// //splice
+
+// myArray.splice(1,3, "AGREGAMOS UN NUEVO ELEMENTO")
+// console.log(myArray)
+
--- 13-ejercicios.js
+++ 13-ejercicios.js
@@ -3,28 +3,57 @@
 //1. Crea un array que almacene cinco animales
 let animals = ["perro", "gato", "ardilla", "caballo", "tortuga"]
 
-//console.log(animals)
+console.log(animals)
 //2. añade dos mas, uno al principio y otro al final
-animals.push("leon")
-animals.unshift("pajaro")
+animals.push("leon") //agrega elementos al final del array
+animals.pop("Gaviota")
 
 console.log(animals)
 //3. Elimina el que se encuentra en la tercera posicion
+delete(animals[3])
+console.log(animals)
+
 
 //4. Crea un set que almacene cinco libros
-
+let books = new Set(["El principito", "CleanCode", "El negociador", "Refactoring", "Three Little Pigs"])
+console.log(books)
 //5. Añade dos más, uno de ellos repetido
+books.add("Blancanieves")
+books.add("Refactoring") // este no se agrega porque esta repetido
 
+console.log(books)
 //6. Elimina uno concreto a tu eleccion
-
+books.delete("CleanCode")
+console.log(books)
 //7. Crea un mapa que asocie el numero del mes a su nombre
-
+let months = new Map ([
+    [1, "Enero"],
+    [2, "Febrero"],
+    [3, "Marzo"],
+    [4, "Abril"],
+    [5, "Mayo"],
+    [6, "Junio"],
+    [7, "Julio"],
+    [8, "Agosto"],
+    [9, "Septiembre"],
+    [10, "Octubre"],
+    [11, "Noviembre"],
+    [12, "Diciembre"]
+])
 //8. Comprueba si el mes numero 5 existe en el map e imrpime su valor
-
+console.log(months.has(5))
+console.log(months.get(5))
 //9. Añaade al mapa una clave con un array como que almacene los meses de verano
-
+months.set("MesesDeVerano", ["Mayo", "Junio", "Julio"])
+console.log(months)
 //10. Crea un array, transformalo a un SET y almacenalo en un MAP.
+let apps = ["MercadoPago", "WhatsApp", "Facebook", "Instagram", "Tiktok"]
+console.log(apps)
+apps = new Set(apps)
+console.log(apps)
 
-//prueba de cambios
-
-//cambios desde la laptop test
+let softwares = new Map([
+    ["Edicion", ["Filmora", "AdobePremiere", "Photoshop"]],
+    ["Array", (apps)]
+])
+console.log(softwares)
